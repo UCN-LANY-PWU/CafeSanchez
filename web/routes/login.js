f0a3c5b6b21a715d8b8e8938f350f49d958fa309
Lars Nysom <lany@ucn.dk>
2024-05-13 13:44:01 +0200

    Added layout and login route

---
+++ web/routes/login.js
@@ -0,0 +1,12 @@
+const express = require('express')
+var router = express.Router();
+
+/* POST handle login attempt */
+router.post('/', function(req, res) {
+
+
+
+
+});
+
+module.exports = router;
